c11e8baac7da44a936090ce0734eb0404c8f80ea
nhoxuan <nhoxuan@atlassian.com>
2021-03-21 21:29:37 +1100

    use router Link and URL param

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,20 @@ import './App.css';
 import { BrowserRouter, Route } from 'react-router-dom';
 import AboutPage from './components/AboutPage';
 import ArticlePage from './components/ArticlePage';
+import ArticlesList from './components/ArticlesList';
 import HomePage from './components/HomePage';
+import NavBar from './components/NavBar';
 
 function App() {
     return (
         <BrowserRouter>
             <div className="App">
+                <NavBar />
                 <div id="page-body">
                     <Route component={HomePage} path="/" exact />
                     <Route component={AboutPage} path="/about" />
-                    <Route component={ArticlePage} path="/article" />
+                    <Route component={ArticlesList} path="/article-list" />
+                    <Route component={ArticlePage} path="/article/:name" />
                 </div>
             </div>
         </BrowserRouter>
--- src/components/ArticlePage.tsx
+++ src/components/ArticlePage.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { RouteComponentProps } from 'react-router-dom';
+import articles from './article-content';
 
-const ArticlePage = () => (
-    <>
-        <h1>This is an article</h1>
-    </>
-);
+const ArticlePage = ({ match }: RouteComponentProps<{ name: string }>) => {
+    const name = match.params.name;
+    const article = articles.find((article) => article.name === name);
+
+    if (!article) return <h1>Not found</h1>;
+
+    return (
+        <>
+            <h1>{article.title}</h1>
+            {article.content.map((para, k) => <p key={k}>{para}</p>)}
+        </>
+    );
+};
 
 export default ArticlePage;
