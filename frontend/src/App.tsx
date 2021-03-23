1a11165fd5c0012199a5fcfac4dbc3023a367412
nhoxuan <nhoxuan@atlassian.com>
2021-03-23 21:35:15 +1100

    refactor, fetch upvote data

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import AboutPage from './components/AboutPage';
-import ArticlePage from './components/ArticlePage';
-import ArticlesList from './components/ArticlesList';
-import HomePage from './components/HomePage';
-import NavBar from './components/NavBar';
-import NotFoundPage from './components/NotFoundPage';
+import AboutPage from './pages/AboutPage';
+import ArticlePage from './pages/ArticlePage';
+import ArticlesListPage from './pages/ArticlesListPage';
+import HomePage from './pages/HomePage';
+import NavBar from './pages/NavBar';
+import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
     return (
@@ -17,7 +17,7 @@ function App() {
                     <Switch>
                         <Route component={HomePage} path="/" exact />
                         <Route component={AboutPage} path="/about" />
-                        <Route component={ArticlesList} path="/article-list" />
+                        <Route component={ArticlesListPage} path="/article-list" />
                         <Route component={ArticlePage} path="/article/:name" />
                         <Route component={NotFoundPage} />
                     </Switch>
---
+++ frontend/src/pages/ArticlePage.tsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import { RouteComponentProps } from 'react-router-dom';
+import ArticlesList from '../components/ArticlesList';
+import articleContent from './article-content';
+import NotFoundPage from './NotFoundPage';
+
+const ArticlePage = ({ match }: RouteComponentProps<{ name: string }>) => {
+    const name = match.params.name;
+    const article = articleContent.find(article => article.name === name);
+
+    const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await fetch(`/api/articles/${name}`);
+            const json = await result.json();
+            setArticleInfo(json);
+        }
+        fetchData();
+    }, [name]);
+
+    if (!article) return <NotFoundPage />
+
+    const otherArticles = articleContent.filter(article => article.name !== name);
+
+    return (
+        <>
+            <h1>{article.title}</h1>
+            <p>This post has been upvoted {articleInfo.upvotes} times</p>
+            {article.content.map((paragraph, key) => (
+                <p key={key}>{paragraph}</p>
+            ))}
+            <h3>Other Articles:</h3>
+            <ArticlesList articles={otherArticles} />
+        </>
+    );
+};
+
+export default ArticlePage;
